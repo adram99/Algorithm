c5f0a65d8f2d3d693ca1160b1f86adc827eeb224
adram99 <102894399+adram99@users.noreply.github.com>
2023-12-19 10:30:51 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#pragma warning(disable:4996)
+
+int main() {
+
+    int n, k;
+    int divisor_cnt = 0;
+
+    scanf("%d %d", &n, &k); // 입력
+
+    int i;
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0) {            // 약수 확인
+            divisor_cnt++;           // 작은순부터 약수 번호 증가
+            if (divisor_cnt == k) {
+                printf("%d", i);
+                break;               // k번째로 작은 약수 확인 -> 종료
+            }
+        }
+        if (i == n) printf("0");     // k번째 약수 존재X
+    }
+
+    return 0;
+}
