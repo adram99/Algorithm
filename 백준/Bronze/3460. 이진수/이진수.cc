7b60bb92ae1fc771e0a3f930f6185f4d5b23866c
adram99 <102894399+adram99@users.noreply.github.com>
2023-12-19 11:06:36 +0900

    [Bronze III] Title: 이진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3460. 이진수/이진수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#pragma warning(disable:4996)
+
+int main() {
+    int testcase, num;
+
+    scanf("%d", &testcase);
+
+    while (testcase > 0) {
+        int one_checker = 0;
+        scanf("%d", &num);
+        while (num != 0) {                                // 2진수로 변환하면서 1위치 찾기
+            if (num % 2 == 1) printf("%d ", one_checker); // 1위치 출력(낮은 위치부터)
+
+            num /= 2;                                     // 다음 자리 계산
+            one_checker++;
+        }
+        printf("\n");
+        testcase--;
+    }
+    return 0;
+}
