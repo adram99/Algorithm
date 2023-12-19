0a1f390f9b25c9e0a3d0f65f03ffad6546269075
adram99 <102894399+adram99@users.noreply.github.com>
2023-12-19 11:54:58 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#pragma warning(disable:4996)
+const int station = 10;
+
+int main() {
+    int outnum, innum, sum, max;
+    sum = 0;
+    max = 0;
+
+    for (int i = 0; i < station; i++) {
+        scanf("%d %d", &outnum, &innum);
+        sum -= outnum;                   // 내릴 때 확인
+        if (sum > max) {
+            max = sum;
+        }
+        sum += innum;                    // 탈 때 확인
+        if (sum > max) {
+            max = sum;
+        }
+        
+    }
+    printf("%d", max);
+    return 0;
+}
