67e81857dd6ab0ea4dd460129789fd61bc252d35
Minsu Seo <102894399+adram99@users.noreply.github.com>
2023-12-22 16:57:14 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#pragma warning (disable:4996)
+using namespace std;
+
+int main() {
+    int start, end, sum;
+    sum = 0;
+
+    scanf("%d %d", &start, &end);
+
+    int num = 1;                              // 생성될 수열의 수
+    int count = 1;                            // 현재 수열 번호
+    while (count <= end) {                    // 끝 지점까지만 수열 
+        for (int i = 0; i < num; i++) {       // 현재 수(num) 만큼 수열 반복
+            if (count >= start) sum += num;
+            count++;
+            if (count > end) break;           // 끝 지점 넘어가면 계산할 필요X
+        }
+        num++;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
