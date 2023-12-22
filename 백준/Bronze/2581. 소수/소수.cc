145d9bea2cdab587ac8087f8285505936c92dd45
Minsu Seo <102894399+adram99@users.noreply.github.com>
2023-12-22 18:01:25 +0900

    [Bronze II] Title: 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#pragma warning (disable:4996)
+using namespace std;
+
+int main() {
+    int start, end, sum, min;
+    sum = 0;
+    min = 0;
+
+    scanf("%d %d", &start, &end);
+
+    for (int i = start; i <= end; i++) {
+        int checker = 0;
+        if (i == 1) continue;               // 1은 소수X
+        for (int j = 2; j * j <= i; j++) {  // 약수가 있는지 확인(에라토스테네스의 체)
+            if (i % j == 0) {
+                checker = 1;
+                break;
+            }
+        }
+        if (checker == 0) {                 // 현재 수(i)가 소수일 때
+            if (min == 0) min = i;
+            sum += i;
+        }
+    }
+    
+    if(sum == 0 && min == 0) printf("-1");
+    else printf("%d\n%d", sum, min);
+
+    return 0;
+}
