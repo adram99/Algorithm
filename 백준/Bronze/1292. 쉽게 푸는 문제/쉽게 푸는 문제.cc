bcaed6d051050af2736615239fb42158512d8d00
Minsu Seo <102894399+adram99@users.noreply.github.com>
2023-12-24 00:33:51 +0900

    Update 쉽게 푸는 문제.cc

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
@@ -8,7 +8,7 @@ int main() {
 
     scanf("%d %d", &start, &end);
 
-    int num = 1;                              // 생성될 수열의 수
+    int num = 1;                              // 현재 수
     int count = 1;                            // 현재 수열 번호
     while (count <= end) {                    // 끝 지점까지만 수열 
         for (int i = 0; i < num; i++) {       // 현재 수(num) 만큼 수열 반복
