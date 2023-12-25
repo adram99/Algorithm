18a42241b8b8561840ca3d070bf7557c75162aa8
Minsu Seo <102894399+adram99@users.noreply.github.com>
2023-12-25 19:42:45 +0900

    Update 연산자 끼워넣기.cc

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -8,27 +8,27 @@ int oper[4];
 int n, res_max = -1000000001, res_min = 1000000001;
 
 void backtracking(int now_res, int next_index) {
-    if (next_index == n) {
+    if (next_index == n) {                       // 계산 완료 후 최대 최소 비교
         res_max = max(res_max, now_res);
         res_min = min(res_min, now_res);
         return;
     }
-    if (oper[0] > 0) {  // +를 선택
+    if (oper[0] > 0) {                           // +를 선택
         oper[0]--;
         backtracking(now_res + num[next_index], next_index + 1);
         oper[0]++;
     }
-    if (oper[1] > 0) {  // -를 선택
+    if (oper[1] > 0) {                           // -를 선택
         oper[1]--;
         backtracking(now_res - num[next_index], next_index + 1);
         oper[1]++;
     }
-    if (oper[2] > 0) {  // *를 선택
+    if (oper[2] > 0) {                           // *를 선택
         oper[2]--;
         backtracking(now_res * num[next_index], next_index + 1);
         oper[2]++;
     }
-    if (oper[3] > 0) {  // /를 선택
+    if (oper[3] > 0) {                           // /를 선택
         oper[3]--;
         backtracking(now_res / num[next_index], next_index + 1);
         oper[3]++;
