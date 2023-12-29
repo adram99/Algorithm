a41ff8733a98d371531ac3e349a7f4878c76455d
Minsu Seo <102894399+adram99@users.noreply.github.com>
2023-12-29 17:58:13 +0900

    Update 가르침.cc

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -4,11 +4,11 @@
 #pragma warning (disable:4996)
 using namespace std;
 
-int abc[26] = { 0, };
-char word[50][16];
-int max_words = 0;
+int abc[26] = { 0, }; // 알파벳 조합 체크용
+char word[50][16];    // 주어진 단어
+int max_words = 0;    // 읽어지는 단어의 최대 개수
 
-int readable(int N) {
+int readable(int N) {                           // 조합된 알파벳(체크된 abc[])로 읽을 수 있는 단어 확인
     int count = 0;
     for (int i = 0; i < N; i++) {
         int check = 1;
@@ -24,10 +24,9 @@ int readable(int N) {
     return max_words;
 }
 
-int dfs(int choose, int index, int N, int K) {
+int dfs(int choose, int index, int N, int K) {  // dfs로 알파벳 조합
     if (choose >= K) {
-        max_words = readable(N);
-        return max_words;
+        return readable(N);
     }
     for (int i = index; i < 26; i++) {
         if (abc[i] == 0) {
@@ -44,28 +43,28 @@ int main() {
 
     scanf("%d %d", &N, &K);
 
-    abc['a' - 'a'] = 1;
+    abc['a' - 'a'] = 1;      // a, c, i, n, t는 무조건 나오므로 미리 체크
     abc['c' - 'a'] = 1;
     abc['i' - 'a'] = 1;
     abc['n' - 'a'] = 1;
     abc['t' - 'a'] = 1;
 
-    K -= 5;
+    K -= 5;                  // dfs에서 a,c,i,n,t를 제외하고 조합하기 위해 K - 5
 
     for (int i = 0; i < N; i++) {
         scanf("%s", &word[i]);
     }
 
-    if (K < 0) {
+    if (K < 0) {             // a,c,i,n,t를 뺀 K값이 0 이하면 읽을 수 있는 단어 없음
         printf("0");
         return 0;
     }
-    else if (K >= 21) {
+    else if (K >= 21) {      // a,c,i,n,t를 뺀 K값이 21 이상이면 모든 단어 다 읽기 가능
         printf("%d", N);
         return 0;
     }
 
-    if (K == 0) {
+    if (K == 0) {            // a,c,i,n,t를 뺀 K값이 0이면 a,c,i,n,t만 포함된 단어만 읽음
         printf("%d", readable(N));
         return 0;
     }
