dc2cd9ad6e70fcd558e6fdd4c2a16e222161fd27
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-01-02 17:18:06 +0900

    Update 멀티탭 스케줄링.cc

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
     int N, K;
-    int hole[100];
-    int nearby[100];
-    int use[100];
+    int hole[100];                      // 구멍
+    int nearby[100];                    // 같은 용품을 다시 사용하기까지 기다리는 시간
+    int use[100];                       // 사용할 용품
 
     scanf("%d %d", &N, &K);
     for (int i = 0; i < N; i++) {
@@ -17,9 +17,9 @@ int main() {
         scanf("%d", &use[i]);
     }
 
-    int count = 0;
+    int count = 0;                      // 플러그 빼는 횟수(= 용품 교체 횟수)
     for (int i = 0; i < K; i++) {
-        int already = 0;
+        int already = 0;                // 이미 꼽힌 용품인지 확인
         for (int j = 0; j < N; j++) {
             if (hole[j] == use[i]) {
                 already = 1;
@@ -29,7 +29,7 @@ int main() {
         if (already == 1) continue;
 
 
-        int empty = 0;
+        int empty = 0;                  // 구멍이 비었는지 확인
         for (int j = 0; j < N; j++) {
             if (hole[j] == 0) {
                 empty = 1;
@@ -40,7 +40,7 @@ int main() {
         if (empty == 1) continue;
         
 
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < N; j++) {   // 다음 사용이 없거나 가장 길게 기다려야하는 용품 확인
             int next = 0;
             for (int k = i+1; k < K; k++) {
                 if (hole[j] == use[k]) break;
@@ -53,7 +53,7 @@ int main() {
             if (nearby[change] < nearby[j]) change = j;
         }
         hole[change] = use[i];
-        count++;
+        count++;                     
     }
     printf("%d", count);
     return 0;
