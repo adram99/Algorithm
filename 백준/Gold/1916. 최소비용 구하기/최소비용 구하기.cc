dddec7d106a1c2750b43fe234d7dd46c459175af
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-01-05 15:59:13 +0900

    Update 최소비용 구하기.cc

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -5,24 +5,27 @@
 using namespace std;
 #define MAX 987654321;
 
-vector<pair<int, int>> graph[1001];
-int dist[1001];
+vector<pair<int, int>> graph[1001];     // int,int 형식으로 저장하는 vector를 그래프로 사용 
+int dist[1001];                         // 도시마다 도착하기까지 드는 최소 비용을 저장
 
 int dijsktra(int start, int end) {
-    dist[start] = 0;
-    priority_queue <pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-    pq.push({ 0, start });
+    dist[start] = 0;                    // 출발지는 비용 0으로 시작
+    priority_queue <pair<int, int>, vector<pair<int, int>>, greater<>> pq;  // 도시, 드는 비용을 넣을 우선순위 큐 선언 (오름차순)
+    pq.push({ start, 0 });              // 출발지와 비용 0
+
     while (!pq.empty()) {
-        int curCost = pq.top().first;
-        int cur = pq.top().second;
+        int curCity = pq.top().first;   // 해당 도시
+        int curCost = pq.top().second;  // 해당 도시까지 드는 비용  
         pq.pop();
-        if (curCost > dist[cur])continue;
-        for (int i = 0; i < graph[cur].size(); i++) {
-            int nextCost = graph[cur][i].second;
-            int next = graph[cur][i].first;
+        if (curCost > dist[curCity]) continue;
+        for (int i = 0; i < graph[curCity].size(); i++) {
+            int nextCost = graph[curCity][i].second;
+            int next = graph[curCity][i].first;
+
             if (dist[next] <= curCost + nextCost) continue;
+
             dist[next] = curCost + nextCost;
-            pq.push({ dist[next],next });
+            pq.push({ dist[next], next });
         }
     }
     return dist[end];
@@ -33,16 +36,16 @@ int main() {
     int start, end, cost;
     int start_want, end_want;
 
-    for (int i = 0; i < 1001; i++) dist[i] = MAX;
+    for (int i = 0; i < 1001; i++) dist[i] = MAX;  // 도시별 드는 최소 요금을 저장하기 위해 처음은 큰 값으로 초기화
 
     scanf("%d", &N);
     scanf("%d", &M);
     for (int i = 0; i < M; i++) {
         scanf("%d %d %d", &start, &end, &cost);
-        graph[start].push_back({ end, cost });
+        graph[start].push_back({ end, cost });     // 주어진 정보로 그래프 생성
     }
     scanf("%d %d", &start_want, &end_want);
-    printf("%d", dijsktra(start_want, end_want));
+    printf("%d", dijsktra(start_want, end_want)); 
 
     return 0;
 }
