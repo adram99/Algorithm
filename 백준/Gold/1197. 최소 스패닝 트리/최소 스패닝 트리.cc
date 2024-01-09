29cd48b609ffb3dd23806e793134c3e7f5ae865a
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-01-09 10:48:11 +0900

    Update 최소 스패닝 트리.cc

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -4,41 +4,41 @@
 #pragma warning (disable:4996)
 using namespace std;
 
-int parent[10001];
+int parent[10001];                 // 부모 노드가 같은지 판단하는 용도
 
 int find(int x) {
     if (parent[x] == x) return x;
-    else return find(parent[x]);
+    else return find(parent[x]);   // 부모 노드가 나올때까지 타고 올라감
 }
 
 void unionNodes(int x, int y) {
     int xRoot = find(x);
     int yRoot = find(y);
-    parent[yRoot] = xRoot;
+    parent[yRoot] = xRoot;         // 노드 연결
 }
 
 int main() {
     int V, E;
     scanf("%d %d", &V, &E);
 
-    for (int i = 0; i <= V; i++) parent[i] = i;
+    for (int i = 0; i <= V; i++) parent[i] = i;    // 아무것도 연결돼있지 않은 상태
 
     int a, b, c;
     vector<pair<int, pair<int, int>>> graph;
     for (int i = 0; i < E; i++) {
         scanf("%d %d %d", &a, &b, &c);
-        graph.push_back({c, {a, b}});
+        graph.push_back({ c, {a, b} });
     }
-    sort(graph.begin(), graph.end());
-    
+    sort(graph.begin(), graph.end());              // 가중치를 기준으로 오름차순 정렬
+
     int sum = 0;
     for (int i = 0; i < E; i++) {
         int W = graph[i].first;
         int node1 = graph[i].second.first;
         int node2 = graph[i].second.second;
-        if (find(node1) == find(node2)) continue;
+        if (find(node1) == find(node2)) continue;  // 부모 노드가 같으면 연결돼있다는 뜻, 패스
         else {
-            unionNodes(node1, node2);
+            unionNodes(node1, node2);              // 연결 안돼있으면 연결
             sum += W;
         }
     }
