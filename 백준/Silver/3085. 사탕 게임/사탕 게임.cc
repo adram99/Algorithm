8ce4dfa501990b2aec07f302466679707bd1a431
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-01-17 12:32:48 +0900

    [Silver II] Title: 사탕 게임, Time: 28 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.cc
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-char board[51][51];
+char board[50][50];
 int max_count = 0;
 
 void change(int y1, int x1, int y2, int x2) {
@@ -11,28 +11,26 @@ void change(int y1, int x1, int y2, int x2) {
 }
 
 void widthSearch(int boardY, int n) { 
+    if (max_count == n) return;
     int count = 1;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n - 1; i++) {
         if (board[boardY][i] == board[boardY][i + 1]) {
             count++;
-        }
-        else {
             if (max_count < count) max_count = count;
-            count = 1;
         }
+        else count = 1;
     }
 }
 
 void heightSearch(int boardX, int n) {
+    if (max_count == n) return;
     int count = 1;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n - 1; i++) {
         if (board[i][boardX] == board[i + 1][boardX]) {
             count++;
-        }
-        else {
             if (max_count < count) max_count = count;
-            count = 1;
         }
+        else count = 1; 
     }
 }
 
@@ -44,21 +42,21 @@ void firstMax(int n) {
 }
 
 void afterChangeMax(char dir, int boardY, int boardX, int n) {
-    firstMax(n);
-    /*
     if (dir == 'w') {
-        widthSearch(boardY, n);
+        /*widthSearch(boardY, n);
         heightSearch(boardX, n);
-        heightSearch(boardX + 1, n);
+        heightSearch(boardX + 1, n);*/
+        firstMax(n);
     }
     else if (dir == 'h') {
-        heightSearch(boardX, n);
+        /*heightSearch(boardX, n);
         widthSearch(boardY, n);
-        widthSearch(boardY + 1, n);
-    }*/
+        widthSearch(boardY + 1, n);*/
+        firstMax(n);
+    }
 }
 
-void changeAndCount(int n) { 
+void changeAndCount(int n) {
     char temp, change_dir;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -66,7 +64,7 @@ void changeAndCount(int n) {
             change_dir = 'w';
             afterChangeMax(change_dir, i, j, n);
             change(i, j, i, j + 1);
-        }
+        } 
     }
     for (int j = 0; j < n; j++) {
         for (int i = 0; i < n; i++) {
@@ -92,13 +90,8 @@ int main() {
     }
 
     firstMax(N);
-    if (max_count == N) {
-        cout << N;
-    }
-    else {
-        changeAndCount(N);
-        cout << max_count;
-    }
+    changeAndCount(N);
+    cout << max_count;
 
     return 0;
 }
