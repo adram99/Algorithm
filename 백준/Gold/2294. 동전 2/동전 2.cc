eb14a8e813ffe4a31a5aae2a7ad8c2d445dba411
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-01-27 16:23:30 +0900

    Update 동전 2.cc

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -8,12 +8,12 @@ int main() {
     int dp[10001];
     int n, k, coin;
     cin >> n >> k;
-    dp[0] = 0;
-    for (int i = 1; i <= k; i++) dp[i] = 10001;
+    dp[0] = 0;      // 0원을 만들기 위한 최소 동전 개수는 0개
+    for (int i = 1; i <= k; i++) dp[i] = 10001;  // 최소값 저장을 위해 초기값은 최대값
     for (int i = 0; i < n; i++) {
         cin >> coin;
         for (int j = coin; j <= k; j++) {
-            if(dp[j] > dp[j - coin] + 1) dp[j] = dp[j - coin] + 1;
+            if(dp[j] > dp[j - coin] + 1) dp[j] = dp[j - coin] + 1;  // 점화식
         }
     }
     if (dp[k] == 10001) cout << -1;
