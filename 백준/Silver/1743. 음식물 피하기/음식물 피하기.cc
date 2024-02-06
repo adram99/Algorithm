25071ef6ed24006a958fdae97f06fc16845b7997
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-02-06 14:57:16 +0900

    [Silver I] Title: 음식물 피하기, Time: 0 ms, Memory: 2128 KB -BaekjoonHub

---
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int map[101][101];
+int visited[101][101];
+int N, M, K, cnt;
+
+int nx[4] = { 0, 0, -1, 1 };
+int ny[4] = { 1, -1, 0, 0 };
+
+void dfs(int y, int x) {
+    cnt++;
+    visited[y][x] = 1;
+
+    for (int i = 0; i < 4; i++) {
+        int next_x = x + nx[i];
+        int next_y = y + ny[i];
+
+        if (next_x < 0 || next_x >= M || next_y < 0 || next_y >= N) continue;
+        else if (map[next_y][next_x] == 1 && visited[next_y][next_x] != 1) dfs(next_y, next_x);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int max = 0;
+    int x, y;
+    cin >> N >> M >> K;
+    for (int i = 0; i < K; i++) {
+        cin >> x >> y;
+        map[x-1][y-1] = 1;
+    }
+    /*
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << map[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    */
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (map[i][j] == 1 && visited[i][j] == 0) {
+                cnt = 0;
+                dfs(i, j);
+                if (cnt > max) max = cnt;
+            }
+        }
+    }
+
+    cout << max;
+    return 0;
+}
