fd475561ed3a2437df4ff4b002f7b8cc4649addf
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-02-07 10:12:07 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> net[101];
+int link[101];
+int cnt;
+
+void dfs(int start) {
+    cnt++;
+    link[start] = 1;
+
+    for (int i = 0; i < net[start].size(); i++) {
+        if (link[net[start][i]] == 0) dfs(net[start][i]);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int com, link;
+    int a, b;
+
+    cin >> com >> link;
+    for (int i = 0; i < link; i++) {
+        cin >> a >> b;
+        net[a].push_back(b);
+        net[b].push_back(a);
+    }
+
+    dfs(1);
+    cout << cnt-1;
+    return 0;
+}
