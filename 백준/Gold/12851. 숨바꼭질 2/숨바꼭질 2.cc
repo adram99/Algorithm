944025c65c7bfbf188f4bd009ed4613e7d52513a
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-02-12 13:46:08 +0900

    Update 숨바꼭질 2.cc

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -8,18 +8,18 @@ int visited[100001];
 
 void bfs(int su, int bro) {
     queue<pair<int, int>> q;
-    q.push(make_pair(0, su));
+    q.push(make_pair(0, su));       // 0초, 수빈의 초기 위치로 시작 
 
     while (!q.empty()) {
         int time = q.front().first;
         int pos = q.front().second;
-        visited[pos] = 1;
+        visited[pos] = 1;           // 방문 체크
         q.pop();
-        if (pos == bro) {
+        if (pos == bro) {           // 목표 위치 도착하면 시간과 횟수 기록
             result = time;
             cnt++;
         }
-        if (visited[bro] == 1 && result < time) {
+        if (visited[bro] == 1 && result < time) {  // 목표 위치에 도착한 기록이 존재하고 탐색중인 시간이 기록된 최소 시간을 넘어가면 탐색 종료
             return;
         }
         if (pos - 1 >= 0 && visited[pos - 1] == 0) q.push(make_pair(time + 1, pos - 1));
@@ -33,7 +33,7 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int subin, brother;
+    int subin, brother;          // 수빈 위치, 동생 위치
     cin >> subin >> brother;
     
     bfs(subin, brother);
