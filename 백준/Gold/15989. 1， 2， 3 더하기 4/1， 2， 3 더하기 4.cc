b6d28598df90dcaee07cfc4096bd2831f8fd059d
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-02-25 11:56:51 +0900

    Update 1， 2， 3 더하기 4.cc

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int dp[10001];
+int dp[10001];  // ex) dp[5]는 5를 1,2,3으로 만드는 방법 수
 
 void dpSet() {
     dp[0] = 1;
-    for (int i = 1; i <= 3; i++) {
+    for (int i = 1; i <= 3; i++) {          // 1로만 만드는 방법, 2를 추가한 방법, 3을 추가한 방법을 더해가면서 계산
         for (int j = i; j <= 10000; j++) {
             dp[j] = dp[j - i] + dp[j];
         }
