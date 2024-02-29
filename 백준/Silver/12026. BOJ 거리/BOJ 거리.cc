dd513e3ed1eb5c0a0de14661ece6732791d124f7
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-02-29 17:35:23 +0900

    Update BOJ 거리.cc

--- 백준/Silver/12026. BOJ 거리/BOJ 거리.cc
+++ 백준/Silver/12026. BOJ 거리/BOJ 거리.cc
@@ -17,16 +17,16 @@ int main() {
     int N;
     string S;
     cin >> N >> S;
-    reset(N);
+    reset(N);      // 최댓값(987654321)으로 초기화
     for (int i = 1; i < N; i++) {
         for (int j = 0; j < i; j++) {
-            if (S[j] == 'B' && S[i] != 'O') continue;
+            if (S[j] == 'B' && S[i] != 'O') continue;      // 앞의 위치(j)가 B인데 뒤의 위치(i)가 O가 아니면 패스
             else if (S[j] == 'O' && S[i] != 'J') continue;
             else if (S[j] == 'J' && S[i] != 'B') continue;
             else jump[i] = min(jump[i], jump[j] + (i - j) * (i - j));
         }
     }
-    if (jump[N - 1] == 987654321) cout << -1;
+    if (jump[N - 1] == 987654321) cout << -1;   // 목표에 도달하지 못했을 경우 
     else cout << jump[N - 1];
 
     return 0;
