2dee20b0c22bebfea6adebc81143403c38b3084e
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-03-06 15:18:45 +0900

    [Gold III] Title: 소형기관차, Time: 4 ms, Memory: 2996 KB -BaekjoonHub

---
+++ 백준/Gold/2616. 소형기관차/소형기관차.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[4][50001];
+int sum[50001];
+
+void DP(int t, int m) {
+    for (int i = 1; i <= 3; i++) {
+        for (int j = i * m; j <= t; j++) {
+            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j - m] + sum[j] - sum[j - m]);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int train, mini, people;
+    cin >> train;
+    for (int i = 1; i <= train; i++) {
+        cin >> people;
+        sum[i] = sum[i - 1] + people;
+    }
+    cin >> mini;
+
+    DP(train, mini);
+    cout << dp[3][train];
+
+    return 0;
+}
