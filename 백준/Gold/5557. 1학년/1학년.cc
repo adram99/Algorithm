3721103166578ed480aace5c865d82b458336878
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-03-09 12:19:29 +0900

    Update 1학년.cc

--- 백준/Gold/5557. 1학년/1학년.cc
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-long long dp[100][21];
+long long dp[100][21];   // ex) dp[n][num]는 n번째 계산 결과가 num이 나올 경우의 수를 의미 
 
 int main() {
     ios::sync_with_stdio(false);
@@ -14,14 +14,14 @@ int main() {
     for (int i = 1; i < N - 1; i++) {
         cin >> num;
         for (int j = 0; j <= 20; j++) {
-            if (dp[i - 1][j] > 0) {
-                if (j - num >= 0) dp[i][j - num] += dp[i - 1][j];
-                if (j + num <= 20) dp[i][j + num] += dp[i - 1][j];
+            if (dp[i - 1][j] > 0) {     // j가 이전 계산으로 나올 수 있는 결과일 때
+                if (j - num >= 0) dp[i][j - num] += dp[i - 1][j];    // num을 빼서 0이상이면 계산 후 기록
+                if (j + num <= 20) dp[i][j + num] += dp[i - 1][j];   // num을 더해서 20이하면 계산 후 기록
             }
         }
     }
     cin >> num;
-    cout << dp[N - 2][num];
+    cout << dp[N - 2][num];  // 0부터 시작하고 마지막 수는 결과로 취급하므로 N-2번째에 num이 나오는 경우가 답
 
     return 0;
 }
