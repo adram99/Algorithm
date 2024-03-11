f7e44185e8264bf3958322442013b1678703d2e9
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-03-11 18:40:09 +0900

    Update 괄호.cc

--- 백준/Gold/10422. 괄호/괄호.cc
+++ 백준/Gold/10422. 괄호/괄호.cc
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-long long dp[5001];
+long long dp[5001];  // ex) dp[i]는 길이가 i인 문자열에서 올바른 괄호의 경우의 수
 
 long long recur(int len) {
     if (len == 0)
@@ -27,7 +27,7 @@ int main() {
     cin >> T;
     for (int i = 0; i < T; i++) {
         cin >> L;
-        if (L % 2 != 0) cout << 0 << '\n';
+        if (L % 2 != 0) cout << 0 << '\n';  // 길이가 짝수면 올바른 괄호 문자열을 만들 수 없음
         else cout << recur(L) << '\n';
     }
 
