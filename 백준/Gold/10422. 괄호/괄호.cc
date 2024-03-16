65281bfa0eafec4ddd882cc61b111b0bab62765b
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-03-17 04:02:18 +0900

    Update 괄호.cc

--- 백준/Gold/10422. 괄호/괄호.cc
+++ 백준/Gold/10422. 괄호/괄호.cc
@@ -4,10 +4,10 @@ using namespace std;
 long long dp[5001];  // ex) dp[i]는 길이가 i인 문자열에서 올바른 괄호의 경우의 수
 
 long long recur(int len) {
-    if (len == 0)
+    if (len == 0)  // len이 0인 경우는 재귀 도중 '()'일 경우를 의미하므로 1 빈환
         return 1;
 
-    if (dp[len] > 0) return dp[len];
+    if (dp[len] > 0) return dp[len];  // 계산했던 값이면 값 반환
 
     for (int i = 2; i <= len; i += 2)
     {
