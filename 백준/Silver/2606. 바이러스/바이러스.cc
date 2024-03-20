ee7daba5d5bffa44e1418b8f10e35306d7d9af09
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-03-20 15:41:56 +0900

    Update 바이러스.cc

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -2,13 +2,13 @@
 #include <vector>
 using namespace std;
 
-vector<int> net[101];
-int link[101];
+vector<int> net[101]; // 그래프를 그리기 위한 2차원 벡터
+int link[101];        // 방문 확인
 int cnt;
 
 void dfs(int start) {
     cnt++;
-    link[start] = 1;
+    link[start] = 1;  // 방문 체크
 
     for (int i = 0; i < net[start].size(); i++) {
         if (link[net[start][i]] == 0) dfs(net[start][i]);
@@ -26,7 +26,7 @@ int main() {
     cin >> coms >> links;
     for (int i = 0; i < links; i++) {
         cin >> a >> b;
-        net[a].push_back(b);
+        net[a].push_back(b);   // a와 b가 연결되면 net[a] = b, net[b] = a 둘다 체크
         net[b].push_back(a);
     }
 
