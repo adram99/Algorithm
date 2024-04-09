74ec73cba3456c6d2b4ee7b2723294d62e30542b
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-04-09 14:30:28 +0900

    [Silver II] Title: LCD Test, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2290. LCD Test/LCD Test.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void lcd(int s, string num) {
+    for (int i = 1; i <= (s * 2 + 3); i++) {
+        for (int j = 0; j < num.size(); j++) {
+            int now = num[j] - 48;
+            if (i == 1) {
+                cout << " ";
+                if (now == 0 || now == 2 || now == 3 || now == 5 || now == 6 || now == 7 || now == 8 || now == 9) {
+                    for (int k = 0; k < s; k++) cout << "-";
+                }
+                else for (int k = 0; k < s; k++) cout << " ";
+                cout << "  ";
+            }
+            else if (i > 1 && i <= s + 1) {
+                if (now == 0 || now == 4 || now == 8 || now == 9) {
+                    cout << "|";
+                    for (int k = 0; k < s; k++) cout << " ";
+                    cout << "|";
+                }
+                else if (now == 1 || now == 2 || now == 3 || now == 7) {
+                    cout << " ";
+                    for (int k = 0; k < s; k++) cout << " ";
+                    cout << "|";
+                }
+                else if (now == 5 || now == 6) {
+                    cout << "|";
+                    for (int k = 0; k < s; k++) cout << " ";
+                    cout << " ";
+                }
+                cout << " "; 
+            }
+            else if (i == s + 2) {
+                cout << " ";
+                if (now == 2 || now == 3 || now == 4 || now == 5 || now == 6 || now == 8 || now == 9) {
+                    for (int k = 0; k < s; k++) cout << "-";
+                }
+                else for (int k = 0; k < s; k++) cout << " ";
+                cout << "  ";
+            }
+            else if (i > s + 2 && i <= s * 2 + 2) {
+                if (now == 0 || now == 6 || now == 8) {
+                    cout << "|";
+                    for (int k = 0; k < s; k++) cout << " ";
+                    cout << "|";
+                }
+                else if (now == 1 || now == 3 || now == 4 || now == 5 || now == 7 || now == 9) {
+                    cout << " ";
+                    for (int k = 0; k < s; k++) cout << " ";
+                    cout << "|";
+                }
+                else if (now == 2) {
+                    cout << "|";
+                    for (int k = 0; k < s; k++) cout << " ";
+                    cout << " ";
+                }
+                cout << " ";
+            }
+            else if (i == s * 2 + 3) {
+                cout << " ";
+                if (now == 0 || now == 2 || now == 3 || now == 5 || now == 6 || now == 8 || now == 9) {
+                    for (int k = 0; k < s; k++) cout << "-";
+                }
+                else for (int k = 0; k < s; k++) cout << " ";
+                cout << "  ";
+            }
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int S;
+    string number;
+    cin >> S >> number;
+    
+    lcd(S, number);
+
+    return 0;
+}
