e7c530c1fad7676ad346adafa57e634d5881ba76
Minsu Seo <102894399+adram99@users.noreply.github.com>
2024-04-16 10:07:44 +0900

    [Silver III] Title: 거북이, Time: 40 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/8911. 거북이/거북이.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+string input;
+vector<pair<int, int>> xy;
+int nextX[4] = { 0, 1, 0, -1 };
+int nextY[4] = { 1, 0, -1, 0 };
+
+void tracking() {
+    int dir = 0;
+    int nowX = 0, nowY = 0;
+    xy.push_back(make_pair(nowX, nowY));
+    for (int i = 0; i < input.size(); i++) {
+        if (input[i] == 'F') {
+            nowX += nextX[dir];
+            nowY += nextY[dir];
+            xy.push_back(make_pair(nowX, nowY));
+        }
+        else if (input[i] == 'B') {
+            nowX -= nextX[dir];
+            nowY -= nextY[dir];
+            xy.push_back(make_pair(nowX, nowY));
+        }
+        else if (input[i] == 'L') {
+            if (dir == 0) dir = 3;
+            else dir--;
+        }
+        else if (input[i] == 'R') {
+            if (dir == 3) dir = 0;
+            else dir++;
+        }
+    }
+}
+
+void calculate() {
+    int min_x = 987654321, min_y = 987654321;
+    int max_x = -987654321, max_y = -987654321;
+    while (xy.size() > 0) {
+        min_x = min(min_x, xy.back().first);
+        max_x = max(max_x, xy.back().first);
+        min_y = min(min_y, xy.back().second);
+        max_y = max(max_y, xy.back().second);
+        xy.pop_back();
+    }
+    cout << (max_x - min_x) * (max_y - min_y) << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> input;
+        tracking();
+        calculate();
+    }
+
+    return 0;
+}
